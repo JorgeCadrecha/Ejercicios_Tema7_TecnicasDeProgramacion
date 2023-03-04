004f49f941190e98ca293aec47e73bdc2b5d58cc
JorgeCadrecha <113858332+JorgeCadrecha@users.noreply.github.com>
2023-03-04 22:44:48 +0000

    Ejercicio 4

--- Ejercicios/Ejercicio4/Ejercicio4.java
+++ Ejercicios/Ejercicio4/Ejercicio4.java
@@ -1,92 +1,50 @@
 package Ejercicio4;
 
 public class Ejercicio4 {
-    
-    public static final int AGUA = 0;
-    public static final int BARCO = 1;
-    public static final int TOCADO = 2;
-    public static final int HUNDIDO = 3;
 
-    
-    private int[][] tablero;
+    public static final int ancho = 4;
+    public static final int largo = 4;
+    public static final char tocado = 'x';
+    public static final char no_tocado = '~';
+    public static final char barco = 'b';
+    public static final char agua = 'a';
+    private char[][] tablero = new char[largo][ancho];
 
-    
     public Ejercicio4() {
-        this.tablero = new int[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                this.tablero[i][j] = AGUA;
+        for (int j = 0; j < largo; j++) {
+            for (int i = 0; i < ancho; i++){
+                this.tablero[j][i] = agua;
             }
         }
     }
 
-    
-    public void depositarBarco(int fila, int columna) {
-        this.tablero[fila][columna] = BARCO;
+    public void posicionarBarco(int x, int y) {
+        this.tablero[y - 1][x - 1] = barco;
     }
 
-    
-    public boolean disparar(int fila, int columna) {
-        if (this.tablero[fila][columna] == BARCO) {
-            this.tablero[fila][columna] = TOCADO;
-            if (this.hundido(fila, columna)) {
-                System.out.println("¡Hundido!");
-                return true;
-            } else {
-                System.out.println("¡Tocado!");
-                return false;
-            }
-        } else {
-            System.out.println("Agua...");
-            return false;
+    public boolean disparar(int x, int y) {
+        if (this.tablero[y - 1][x - 1] != barco)
+        {
+            this.tablero[y - 1][x - 1] = no_tocado;
         }
+        else {
+            this.tablero[y - 1][x - 1] = tocado;
+        }
+        return this.tablero[y - 1][x - 1] == tocado;
     }
 
-    
-    private boolean hundido(int fila, int columna) {
-        int tamano = 1;
-        while (fila + tamano < 10 && this.tablero[fila + tamano][columna] == TOCADO) {
-            tamano++;
-        }
-        if (fila + tamano < 10 && this.tablero[fila + tamano][columna] == BARCO) {
-            return false;
-        }
-        tamano = 1;
-        while (columna + tamano < 10 && this.tablero[fila][columna + tamano] == TOCADO) {
-            tamano++;
-        }
-        if (columna + tamano < 10 && this.tablero[fila][columna + tamano] == BARCO) {
-            return false;
-        }
-        return true;
-    }
 
-    
-    public void visualizacion() {
-        System.out.print("  ");
-        for (int j = 0; j < 10; j++) {
-            System.out.print(j + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < 10; j++) {
-                switch (this.tablero[i][j]) {
-                    case AGUA:
-                        System.out.print(". ");
-                        break;
-                    case BARCO:
-                        System.out.print("O ");
-                        break;
-                    case TOCADO:
-                        System.out.print("X ");
-                        break;
-                    case HUNDIDO:
-                        System.out.print("# ");
-                        break;
+    public void mostrar() {
+        for (int j = 0; j < largo; j++) {
+            for (int i = 0; i < ancho; i++)
+                if (this.tablero[j][i] == barco || this.tablero[j][i] == agua) {
+                    System.out.print("?");
                 }
-            }
+                else {
+                    System.out.print(this.tablero[j][i]);
+                }
             System.out.println();
         }
     }
+
 }
--- Ejercicios/Ejercicio4/Main4.java
+++ Ejercicios/Ejercicio4/Main4.java
@@ -1,39 +1,108 @@
 package Ejercicio4;
-
-
-import java.util.Scanner;
+import java.util.*;
 
 public class Main4 {
+    private static Scanner console = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Ejercicio4 tablero_Jugardor, tablero_Ordenador;
+        boolean gana;
 
-        
-        Ejercicio4 cuadricula = new Ejercicio4();
+        tablero_Ordenador = new Ejercicio4();
+        Random random = new Random();
+        int x = random.nextInt(Ejercicio4.ancho) + 1;
+        int y = random.nextInt(Ejercicio4.largo) + 1;
+        tablero_Ordenador.posicionarBarco(x, y);
 
-        
-        int filaBarco = (int) (Math.random() * 10);
-        int columnaBarco = (int) (Math.random() * 10);
-        cuadricula.depositarBarco(filaBarco, columnaBarco);
-        System.out.println("Se ha colocado un barco en la fila " + filaBarco + ", columna " + columnaBarco + ".");
 
-        
-        boolean hundido = false;
-        while (!hundido) {
-            
-            cuadricula.visualizacion();
+        tablero_Jugardor = new Ejercicio4();
+        System.out.println("Ubique su barco");
+        x = introducir("¿Qué columna?(Eje X)", 1, Ejercicio4.ancho);
+        y = introducir("¿Qué fila?(Eje Y)", 1, Ejercicio4.largo);
+        tablero_Jugardor.posicionarBarco(x, y);
 
-            
-            System.out.print("Introduce la fila donde quieres disparar (0-9): ");
-            int filaDisparo = scanner.nextInt();
-            System.out.print("Introduce la columna donde quieres disparar (0-9): ");
-            int columnaDisparo = scanner.nextInt();
+        boolean[][] aleatorio = new boolean[Ejercicio4.ancho][Ejercicio4.largo];
 
-            
-            hundido = cuadricula.disparar(filaDisparo, columnaDisparo);
-        }
+        do {
 
-       
-        cuadricula.visualizacion();
-        System.out.println("¡Has hundido el barco!");
+            System.out.println("Tu tabla de disparo: (Es la que repesenta tus disparos)");
+            tablero_Ordenador.mostrar();
+            x = introducir("¿Qué columna?(Eje X)", 1, Ejercicio4.ancho);
+            y = introducir("¿Qué fila?(Eje Y)", 1, Ejercicio4.largo);
+            gana = tablero_Ordenador.disparar(x, y);
+
+            if (gana) {
+                System.out.println("Tocado y hundido. Ha ganado");
+                tablero_Ordenador.mostrar();
+            } else {
+                 do {
+                        x = random.nextInt(Ejercicio4.ancho) + 1;
+                        y = random.nextInt(Ejercicio4.largo) + 1;
+                 }while (aleatorio[x-1][y-1]);
+                    aleatorio[x-1][y-1] = true;
+
+                 x = random.nextInt(Ejercicio4.ancho) + 1;
+                 y = random.nextInt(Ejercicio4.largo) + 1;
+
+                System.out.println("El ordenador disparó en " + x + "(Eje X); " + y + "(Eje Y)");
+
+                gana = tablero_Jugardor.disparar(x, y);
+
+                System.out.println("Tabla de disparo del ordenador: (Es la que repesenta los disparos del ordenador)");
+                tablero_Jugardor.mostrar();
+
+                if (gana) {
+                    System.out.println("Tocado y hundido. Ha perdido");
+                }
+            }
+        } while (!gana);
     }
+
+    public static int introducir(String mensaje, int min, int max) {
+        int valor;
+
+        System.out.println(mensaje);
+        valor = console.nextInt();
+        console.nextLine();
+
+        while (valor < min || valor > max) {
+            System.out.println("El valor debe estar comprendido entre " + min + " y " + max);
+            System.out.println(mensaje);
+            valor = console.nextInt();
+            console.nextLine();
+        }
+        return valor;
+    }
+
+    //Cordenadas del barco de ordenador que no se repitan
+
+    private  Set<String> generarCordenadas;
+    private  Random random;
+
+    public  void CordenadasAleatorias() {
+        generarCordenadas = new HashSet<>();
+        random = new Random();
+    }
+    public int CordenadasAleatoriasX(int min, int max)
+    {
+    return random.nextInt(max - min + 1) + min;
+    }
+    public int CordenadasAleatoriasY(int min, int max)
+    {
+    return random.nextInt(max - min + 1) + min;
+    }
+    public String CordenadasAleatoriasXY(int min, int max)
+    {
+        String cordenadas = null;
+    do {
+        int x = CordenadasAleatoriasX(min, max);
+        int y = CordenadasAleatoriasY(min, max);
+    }while (generarCordenadas.contains(cordenadas));
+    generarCordenadas.add(cordenadas);
+        return cordenadas;
+    }
+
+
 }
+
+
